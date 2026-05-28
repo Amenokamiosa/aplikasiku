306d1a11d57c1a184ece22c4d10dfe51a4d4e0d5
Amenokamiosa <amenokamiosaka@gmail.com>
2026-05-28 18:59:57 +0700

    second commit

--- resources/views/welcome1.blade.php
+++ resources/views/welcome1.blade.php
@@ -8,30 +8,20 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-sRIl4kxILFvY47J16cr9ZwB07vP4J8+LH7qKQnuqkuIAvNWLzeN8tE5YBujZqJLB" crossorigin="anonymous">
 </head>
 <body>
-    <div class="menu">
-                <div class="row">
-        <div class="col-4">
-            <div class="list-group" id="list-tab" role="tablist">
-            <a class="list-group-item list-group-item-action active" id="list-home-list" data-bs-toggle="list" href="#list-home" role="tab" aria-controls="list-home">Home</a>
-            <a class="list-group-item list-group-item-action" id="list-profile-list" data-bs-toggle="list" href="#list-profile" role="tab" aria-controls="list-profile">Profile</a>
-            <a class="list-group-item list-group-item-action" id="list-messages-list" data-bs-toggle="list" href="#list-messages" role="tab" aria-controls="list-messages">Messages</a>
-            <a class="list-group-item list-group-item-action" id="list-settings-list" data-bs-toggle="list" href="#list-settings" role="tab" aria-controls="list-settings">Settings</a>
-            </div>
+    <div class="main-frame">
+        <div class="sidebar-panel">
+            <!-- Button list -->
+            <li>
+                <ul>Home</ul>
+                <ul>Journal</ul>
+                <ul>Donate</ul>
+                <ul>Schedule</ul>
+                <ul>Document</ul>
+                <ul>Account</ul>
+                <ul>More</ul>
+            </li>
         </div>
-        <div class="col-8">
-            <div class="tab-content" id="nav-tabContent">
-            <div class="tab-pane fade show active" id="list-home" role="tabpanel" aria-labelledby="list-home-list">...</div>
-            <div class="tab-pane fade" id="list-profile" role="tabpanel" aria-labelledby="list-profile-list">...</div>
-            <div class="tab-pane fade" id="list-messages" role="tabpanel" aria-labelledby="list-messages-list">...</div>
-            <div class="tab-pane fade" id="list-settings" role="tabpanel" aria-labelledby="list-settings-list">...</div>
-            </div>
-        </div>
-</div>
+        <div class="maiin-panel"></div>
     </div>
-    <div class="container">
-        <div class="panel">
-        </div>
-    </div>
-
 </body>
 </html>
